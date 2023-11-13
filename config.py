e3409fdc01ce3c76ffe843ab4a459f5dcc34187e
SHAKTI BIPLAB <85693629+LUCIFERGREYHAT1@users.noreply.github.com>
2023-11-13 20:52:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+{
+    "token": "MTE2NzE5NzM5NzIzMTg2MTc3MA.GyZvBn.3dyVsjQJG5nhevTyk8hxXsQSfCeF4TF9-AOGh0",
+    "proxy": false,
+    "nuke": {
+        "channels_name": [
+            "LUCIFER THE KING OF HELL ",
+            "The Fucking kid Here",
+            "hell raiders",
+            "Hell nukers"
+        ],
+        "roles_name": [
+            "wizzed by Aizer x vairus ",
+            "Nukers Territory",
+            "Aizer x virus  Papa Here"
+        ],
+        "messages_content": [
+            "@everyone @here **LUCIFER SE PANGA NEI** https://discord.gg/sgVK8Shp98",
+            "@everyone @here **LUCIFER SE PANGA NEI** https://discord.gg/sgVK8Shp98",
+            "@everyone @here **LUCIFER SE PANGA NEI** https://discord.gg/sgVK8Shp98"
+        ]
+    }
+}
